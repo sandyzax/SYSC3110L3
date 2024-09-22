fa307f9843a8208c58f4cda998b6b5d208fd1026
Sandy <sandybillyo@gmail.com>
2024-09-22 19:35:11 -0400

    The Address Book class has been changed

---
+++ src/BuddyInfo.java
@@ -0,0 +1,44 @@
+public class BuddyInfo {
+
+    private String name;
+    private int age;
+
+    public BuddyInfo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public BuddyInfo() {
+
+        this.name = "Homer";
+        this.age = 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        BuddyInfo Homer = new BuddyInfo();
+        BuddyInfo Mary = new BuddyInfo("Mary",18);
+        //System.out.println("Hello World");
+        System.out.println(Homer.getName());
+        BuddyInfo buddy = new BuddyInfo("Tom",22);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+        addressBook.addBuddy(Mary);
+        addressBook.removeBuddy(Mary);
+    }
+}
